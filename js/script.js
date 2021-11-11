074a605402248263fcacceb0a7995eaec5b51024
Tatiana <itstepkashpruk@gmail.com>
2021-11-11 15:38:06 +0200

    Installing, use  axios

--- js/script.js
+++ js/script.js
@@ -188,13 +188,19 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         return await res.json();
        };
-       getResource('http://localhost:3000/menu')
-       .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-          new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-         });
-       });
+      //  getResource('http://localhost:3000/menu')
+      //  .then(data => {
+      //   data.forEach(({img, altimg, title, descr, price}) => {
+      //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      //    });
+      //  });
 
+      axios.get('http://localhost:3000/menu')
+      .then(data => {
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+          new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+          });
+      });
       //FORMS
 
       const forms = document.querySelectorAll('form');
@@ -250,7 +256,6 @@ window.addEventListener('DOMContentLoaded', () => {
           });
         });
       }
-
 
       function showThanksModal(message){
         const prevModalDialog = document.querySelector('.modal__dialog');
